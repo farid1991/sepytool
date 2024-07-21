158b0f25dd434be5ff96357ee5826bbeba63710c
Vadim Yanitskiy <fixeria@osmocom.org>
2024-07-22 01:25:36 +0700

    sepytool.py: argparse improvements

--- sepytool.py
+++ sepytool.py
@@ -6,26 +6,26 @@ import argparse
 
 __version__ = '0.1.1'
 
+ap = argparse.ArgumentParser(prog='sepytool',
+                             description='Ericsson and Sony Ericsson Tool')
+sp = ap.add_subparsers(dest='command', metavar='command', required=True,
+                       help='sub-command help')
 
-def setool():
-    parser = argparse.ArgumentParser(description='Ericsson and Sony Ericsson Tool')
-    # Add the arguments
-    parser.add_argument('-p', '--port', help='Set serial port')
-    parser.add_argument('-b', '--baudrate', type=int, help='Set baudrate')
-    parser.add_argument('-e', '--execute', type=str, help='Execute action')
+# global arguments
+ap.add_argument('-p', '--port', type=str, required=True,
+                help='Set serial port (e.g. /dev/ttyUSB0 or COM1)')
+ap.add_argument('-b', '--baudrate', type=int, default=115200,
+                choices=[115200, 230400, 460800, 921600],
+                help='Set baudrate (default: %(default)s)')
 
-    # Parse the command-line arguments
-    args = parser.parse_args()
-
-    # Check if no arguments provided
-    if not any(vars(args).values()):
-        parser.print_help()
-        return
-    
-    # Process the arguments
-    flasher = Flasher(args.port, args.baudrate, args.execute)
-    flasher.wait_for_answer()
-    flasher.do_job()
+# identification
+ident = sp.add_parser('identify',
+                      help='Identify connected phone')
 
 if __name__ == '__main__':
-    setool()
+    # Parse the command-line arguments
+    args = ap.parse_args()
+    # Process the arguments
+    flasher = Flasher(args.port, args.baudrate, args.command)
+    flasher.wait_for_answer()
+    flasher.do_job()
